2b7652abb00eb01f808bdfdf546baa2f37c813ac
Sufian681 <sufiansonic@gmail.com>
2026-06-09 19:22:44 +0900

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,13 @@
+file = open('Codingal.txt','r')
+print(file.read())
+file.close()
+
+file = open('Codingal.txt','r')
+print("\n Read in parts \n")
+print(file.read(8))
+file.close()
+
+file = open('Codingal.txt','a')
+file.write(" Hi! This is written by Ms. Prachi")
+file.close()
+
